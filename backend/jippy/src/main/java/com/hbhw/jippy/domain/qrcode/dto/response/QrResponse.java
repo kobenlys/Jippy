9880036928f3b2ad886d9f88ec57236499e4d184
강지민 <kjmemail2743@gmail.com>
2025-02-05 16:18:24 +0900

    feat: QR 관련 Controller 구현

---
+++ backend/jippy/src/main/java/com/hbhw/jippy/domain/qrcode/dto/response/QrResponse.java
@@ -0,0 +1,6 @@
+package com.hbhw.jippy.domain.qrcode.dto.response;
+
+public class QrResponse {
+    private Long qrId;
+    private String description;
+}
---
+++ backend/jippy/src/main/java/com/hbhw/jippy/domain/qrcode/service/QrService.java
@@ -0,0 +1,10 @@
+package com.hbhw.jippy.domain.qrcode.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class QrService {
+
+}
