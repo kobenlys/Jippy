57e1e48fb3a472bc950d3f6760c1c9cb3b0aca4c
이영석 <vlfxhd69@naver.com>
2025-02-14 11:33:53 +0900

    feat: product 테스트코드 작성
    세트메뉴서비스 테스트코드 작성

--- backend/jippy/src/test/java/com/hbhw/jippy/SetMenuServiceTest.java
+++ backend/jippy/src/test/java/com/hbhw/jippy/SetMenuServiceTest.java
@@ -1,7 +1,10 @@
 package com.hbhw.jippy;
 
 import com.hbhw.jippy.domain.product.dto.request.CreateSetMenuRequest;
+import com.hbhw.jippy.domain.product.dto.request.UpdateSetMenuRequest;
 import com.hbhw.jippy.domain.product.dto.response.ProductListResponse;
+import com.hbhw.jippy.domain.product.dto.response.SetMenuInfoResponse;
+import com.hbhw.jippy.domain.product.dto.response.SetMenuResponse;
 import com.hbhw.jippy.domain.product.entity.Product;
 import com.hbhw.jippy.domain.product.entity.ProductCategory;
 import com.hbhw.jippy.domain.product.entity.SetMenu;
@@ -16,6 +19,7 @@ import com.hbhw.jippy.domain.store.entity.Store;
 import com.hbhw.jippy.domain.store.service.StoreService;
 import com.hbhw.jippy.domain.user.entity.UserOwner;
 import com.hbhw.jippy.domain.user.enums.StaffType;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -27,6 +31,7 @@ import org.mockito.quality.Strictness;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -44,6 +49,28 @@ public class SetMenuServiceTest {
     private ProductService productService;
     @Mock
     private StoreService storeService;
+
+    private SetMenu setMenu;
+    private UpdateSetMenuRequest updateRequest;
+
+    @BeforeEach
+    void setUp() {
+        setMenu = SetMenu.builder()
+                .id(1)
+                .image("setmenu1.img")
+                .name("브런치세트")
+                .price(8800)
+                .setMenuConfigList(new ArrayList<>())
+                .build();
+
+        updateRequest = new UpdateSetMenuRequest();
+        updateRequest.setSetMenuId(1);
+        updateRequest.setStoreId(1);
+        updateRequest.setName("New Name");
+        updateRequest.setImage("new-image.png");
+        updateRequest.setPrice(2000);
+        updateRequest.setProductIdList(List.of(10L, 20L));
+    }
 
     @Test
     void createSetMenu_Success(){
@@ -131,5 +158,147 @@ public class SetMenuServiceTest {
         assertEquals(3500, mockSetMenu.getSetMenuConfigList().get(1).getProduct().getPrice());
         assertEquals(8800, mockSetMenu.getPrice());
         verify(setMenuRepository, times(1)).save(any(SetMenu.class));
+    }
+
+    @Test
+    void modifySetMenu_success() {
+
+        Product mockProduct = mock(Product.class);
+        SetMenu mockSetMenu = mock(SetMenu.class);
+        when(mockSetMenu.getPrice()).thenReturn(2000);
+        when(mockSetMenu.getName()).thenReturn("New Name");
+        when(mockSetMenu.getImage()).thenReturn("new-image.png");
+
+        SetMenuConfig mockNewSetMenuConfig = SetMenuConfig.builder()
+                .setMenu(mockSetMenu)
+                .product(mockProduct)
+                .build();
+
+        when(setMenuRepository.findByIdAndStoreId(anyInt(), anyInt())).thenReturn(Optional.ofNullable(setMenu));
+        when(setMenuConfigService.getOrCreateSetMenuConfig(setMenu, 1, 1L))
+                .thenReturn(mockNewSetMenuConfig);
+
+        setMenuService.modifySetMenu(updateRequest);
+
+        assertNotNull(setMenu);
+        assertEquals("New Name", setMenu.getName());
+        assertEquals("new-image.png", setMenu.getImage());
+        assertEquals(2000, setMenu.getPrice());
+        assertEquals(2, setMenu.getSetMenuConfigList().size());
+    }
+
+    @Test
+    void selectDetailSetMenuList_success() {
+        Integer storeId = 100;
+        List<SetMenu> setMenuList = new ArrayList<>();
+
+        SetMenu setMenu1 = mock(SetMenu.class);
+        when(setMenu1.getName()).thenReturn("set1_name");
+        when(setMenu1.getImage()).thenReturn("set1_image");
+        when(setMenu1.getPrice()).thenReturn(5000);
+
+        SetMenu setMenu2 = mock(SetMenu.class);
+        when(setMenu2.getName()).thenReturn("set2_name");
+        when(setMenu2.getImage()).thenReturn("set2_image");
+        when(setMenu2.getPrice()).thenReturn(7000);
+        setMenuList.add(setMenu1);
+        setMenuList.add(setMenu2);
+
+        when(setMenuRepository.findByStoreId(storeId)).thenReturn(setMenuList);
+        SetMenuInfoResponse response = setMenuService.selectDetailSetMenuList(storeId);
+
+        assertNotNull(response);
+        assertEquals(2, response.getSetMenuList().size());
+        assertEquals("set1_name", response.getSetMenuList().get(0).getName());
+        assertEquals("set1_image", response.getSetMenuList().get(0).getImage());
+        assertEquals(5000, response.getSetMenuList().get(0).getPrice());
+        assertEquals("set2_name", response.getSetMenuList().get(1).getName());
+        assertEquals("set2_image", response.getSetMenuList().get(1).getImage());
+        assertEquals(7000, response.getSetMenuList().get(1).getPrice());
+    }
+
+    public Store getMockStore(){
+        SetMenu mockSetMenu = SetMenu.builder()
+                .id(1)
+                .image("setmenu1.img")
+                .name("브런치세트")
+                .price(8800)
+                .setMenuConfigList(new ArrayList<>())
+                .build();
+        UserOwner mockUserOwner1 = new UserOwner("owner1@example.com", "password1", "김싸피", "1999-01-15", StaffType.OWNER, "fcmToken1");
+        // 매장 ID
+        // 점주 정보
+        return Store.builder()
+                .id(1)  // 매장 ID
+                .userOwner(mockUserOwner1)  // 점주 정보
+                .name("테스트 매장")
+                .address("서울특별시 강남구 테헤란로 123")
+                .openingDate("2024-01-01")
+                .totalCash(1000000)
+                .businessRegistrationNumber("123-45-67890")
+                .build();
+    }
+
+    public Product getMockProduct1(){
+        Store mockStore = getMockStore();
+        return Product.builder()
+                .id(1L)
+                .store(mockStore)
+                .name("아메리카노")
+                .price(4500)
+                .status(true)
+                .image("americano.jpg")
+                .productType(ProductType.ICE) // 가정: ProductType Enum 값
+                .productSize(ProductSize.L) // 가정: ProductSize Enum 값
+                .store(Store.builder()
+                        .id(1)
+                        .name("카페 하루")
+                        .build()
+                )
+                .productCategory(ProductCategory.builder()
+                        .id(2)
+                        .productCategoryName("커피")
+                        .build()
+                )
+                .build();
+    }
+
+    public Product getMockProduct2() {
+        Store mockStore = getMockStore();
+        return Product.builder()
+                .id(2L)
+                .store(mockStore)
+                .name("카페라떼")
+                .price(3500)
+                .status(true)
+                .image("caffelatte.jpg")
+                .productType(ProductType.ICE) // 가정: ProductType Enum 값
+                .productSize(ProductSize.L) // 가정: ProductSize Enum 값
+                .store(Store.builder()
+                        .id(1)
+                        .name("카페 하루")
+                        .build()
+                )
+                .productCategory(ProductCategory.builder()
+                        .id(2)
+                        .productCategoryName("커피")
+                        .build()
+                )
+                .build();
+    }
+
+    public List<SetMenuConfig> getMockSetMenuConfigList(){
+        Product product1 = getMockProduct1();
+        Product product2 = getMockProduct2();
+
+        SetMenuConfig mockSetMenuConfig1 = mock(SetMenuConfig.class);
+        when(mockSetMenuConfig1.getSetMenu()).thenReturn(setMenu);
+        when(mockSetMenuConfig1.getProduct()).thenReturn(product1);
+
+        SetMenuConfig mockSetMenuConfig2 = mock(SetMenuConfig.class);
+        when(mockSetMenuConfig2.getSetMenu()).thenReturn(setMenu);
+        when(mockSetMenuConfig2.getProduct()).thenReturn(product2);
+
+        return Arrays.asList(mockSetMenuConfig1, mockSetMenuConfig2);
     }
 }
