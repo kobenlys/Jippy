1eb62287b256bf8ed37d0656334523247e1f5c21
vlfxhd69@naver.com <vlfxhd69@nvaer.com>
2025-02-16 15:08:06 +0900

    fix: 유저 회원가입 기능 수정

--- backend/jippy/src/main/java/com/hbhw/jippy/domain/user/entity/UserOwner.java
+++ backend/jippy/src/main/java/com/hbhw/jippy/domain/user/entity/UserOwner.java
@@ -7,21 +7,22 @@ import jakarta.persistence.Convert;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import lombok.*;
+import lombok.experimental.SuperBuilder;
 
 @Entity
 @Table(name = "user_owner")
 @Getter
 @AllArgsConstructor
-@Builder
+@SuperBuilder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class UserOwner extends BaseUser {
     @Convert(converter = StaffTypeConverter.class)
     @Column(name = "staff_type", nullable = false)
     private StaffType staffType;
 
-    @Builder
-    public UserOwner(String email, String password, String name, String age, StaffType staffType, String fcmToken) {
-        super(email, password, name, age, fcmToken);
-        this.staffType = staffType;
-    }
+//    @Builder
+//    public UserOwner(String email, String password, String name, String age, StaffType staffType, String fcmToken) {
+//        super(email, password, name, age, fcmToken);
+//        this.staffType = staffType;
+//    }
 }
--- backend/jippy/src/main/java/com/hbhw/jippy/domain/user/service/UserService.java
+++ backend/jippy/src/main/java/com/hbhw/jippy/domain/user/service/UserService.java
@@ -71,6 +71,7 @@ public class UserService {
         }
 
         BaseUser newUser = userFactory.createUser(request, UserType.OWNER);
+        log.info("newUser {}", newUser);
         userOwnerRepository.save((UserOwner) newUser);
     }
 
